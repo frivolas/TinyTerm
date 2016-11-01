85484540edb0bb9fda61773d0abfc763586495c8
Oscar Frias <oscar@oscarfrias.com>
2016-10-31 22:18:46 -0400

    Serial connection manager
    Added a first pass at trying to control the serial connection so it's
    not hard-coded. Need to test it in different 'puters.

--- TinyTerm.pde
+++ TinyTerm.pde
@@ -22,6 +22,10 @@
 // The GUI gives you a textArea to show the history of what has been sent and received
 // and a text box to type and inject GCode or tinyG Commands on the fly into the tinyG
 // The interface is created using the controlP5 library.
+//
+// Thanks to http://stackoverflow.com/questions/29107544/serial-com-port-selection-by-dropdownlist-in-processing
+// for the help with adding the Serial port dropdown list
+//
 
 
 // Imports
@@ -31,10 +35,12 @@ import processing.serial.*;
 // GUI
 ControlP5 cp5;
 boolean buttonFlag=false;
+ScrollableList serialPortsList;
 
 // String variables
 String theGCode = "G91 G1 X100 F100\n";     // Whatever you want to have as default text in the textbox
 String jPath = "init.json";                 // the path where the JSON file is
+String fileToDump;                          // String to store filename with absolute path of last dumped file (to quickly re-dump)
 
 // The Init.JSON file to be loaded
 JSONObject initFile;      // This will receive the JSON file as an object
@@ -45,6 +51,8 @@ PrintWriter logFile;
 
 // The serial port:
 Serial myPort;  // Create object from Serial class
+int BAUD_RATE = 9600;
+Boolean serialConnected = false;
 
 //misc variables
 int x = 50;             // Position on the X axis
@@ -58,6 +66,7 @@ int theWidth = 800;     // applet width
 int theHeight = 600;    // applet height
 int pad = 20;           // padding between fields
 int lineCounter = 0;    // linefeed char coming from the serial
+boolean tinyGconnected = true;
 
 Textarea myTerminal;    // CP5 control for the text area
 PFont font;             // the font for the script
@@ -68,21 +77,27 @@ void settings(){
 }
 
 
-void setup()
-{
+void setup(){
   // size(600,600);      //Just if you have a really old version of Processing. Like this laptop.
   // Start the serial
-  // List all the available serial ports, check the terminal window and select find the port# for the tinyG
-  printArray(Serial.list());
-  // Open whichever port the tinyG uses in your computer (8 in mine):
-  myPort = new Serial(this, Serial.list()[2], 9600);
-  myPort.clear();
-
   font = createFont("arial", 20); // big arial font
   startGUI();
   cp5.get(Bang.class,"loadFile").setTriggerEvent(Bang.RELEASE); // make the bang react at release
   // Gui Loaded. Terminal ready
+
+  // List all the available serial ports, check the terminal window and select find the port# for the tinyG
+  String[] portNames = Serial.list();
+  printArray(portNames);
+  // Open whichever port the tinyG uses in your computer (8 in mine):
+  for(int i=0;i<portNames.length;i++){
+    serialPortsList.addItem(portNames[i], i);
+  }
+
+
+  guiHide();
+
   myTerminal.append(theTime() + "Terminal ready... \n");
+  myTerminal.append(theTime() + "Please choose a Serial port to connect to... \n");
   myTerminal.scroll(1);
   textFont(font);
 }
@@ -92,6 +107,7 @@ void setup()
 void draw() {
   background(0);  //black BG
   //read response from tinyG
+  if(myPort != null){
   while (myPort.available () > 0) {
     String inBuffer = myPort.readString();
     if (inBuffer != null) {
@@ -111,10 +127,12 @@ void draw() {
       myTerminal.scroll(1);         // scroll to the bottom of the terminal
     }
   }
+    myPort.clear();
+}
   fill(255);
   stroke(255);
   text("TinyTerm:", x, y-pad);
-  myPort.clear();
+
 }
 
 
@@ -135,13 +153,41 @@ void controlEvent(ControlEvent theEvent) {
         myTerminal.append(theTime() + "Terminal ready...\n");
       }
       else{
-      myPort.write(theGCode);
-      myTerminal.append(theTime() + theGCode);
-      myTerminal.scroll(1);
-    }
+        myPort.write(theGCode);
+        myTerminal.append(theTime() + theGCode);
+        myTerminal.scroll(1);
+      }
     }
   }
 }
+
+
+
+void serialports(int n){
+  // Stop any active connections
+  if(myPort != null){
+    myPort.stop();
+    myPort = null;
+  }
+  // open the selected port
+  try{
+    myPort = new Serial(this, Serial.list()[n], BAUD_RATE);
+    serialConnected = true;
+    } catch (Exception e) {
+      System.err.println("Error opening serial port " + myPort);
+      myTerminal.append(theTime() + "Error opening the selected serial port... \n");
+      serialConnected = false;
+      e.printStackTrace();
+    }
+    if(serialConnected) {
+      println("Yay Serial!");
+      myTerminal.append(theTime() + "Connected to Serial on port COM" + n + "\n");
+      guiShow();
+  } else {
+    println("Boo no Serial");
+    myTerminal.append(theTime() + "No Serial connection" + "\n");
+  }
+  }
 
 
 public void Send(){
@@ -171,29 +217,6 @@ public void clear() {
   cp5.get(Textfield.class,"input").clear();
 }
 
-
-
-// When the load file bang is released, open a file explorer window
-void loadFile(){
-  myTerminal.append(theTime() + "Loading file...\n");
-  selectInput("Select script file to load", "fileLoaded");
-}
-
-// See what the user selected as a file
-void fileLoaded(File selection){
-  // If no file, print on screen.
-  if(selection == null){
-    // println("No file selected");
-    myTerminal.append(theTime() + "No file selected\n");
-  } else {
-    // If file, say it and send the file to the dumpFile function
-    // println("File to load: " + selection.getAbsolutePath());
-    myTerminal.append(theTime() + "File to load: " + selection.getAbsolutePath() + "\n");
-    dumpFile(selection.getAbsolutePath());
-  }
-  // remove the callback from the Bang or else it never lets us go
-  cp5.get(Bang.class,"loadFile").removeCallback();
-}
 
 
 // Let's work on the GUI
@@ -254,6 +277,15 @@ void startGUI(){
   .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
   ;
 
+  // create a new button to re-dump the last opened file to the tinyG
+  cp5.addBang("againFile")
+  .setCaptionLabel("Re-Dump File")
+  .setPosition(x+taw+pad, y+tah+pad)
+  .setSize(bw, tfh)
+  .setColorBackground(color(180,40,50))
+  .setColorActive(color(180,40,50))
+  .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
+  ;
 
   // create a new button to save the log
   cp5.addBang("saveLog")
@@ -277,6 +309,56 @@ void startGUI(){
   .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
   ;
 
+  serialPortsList = cp5.addScrollableList("serialports")
+  .setPosition(x+taw+pad,y+(3*sbh)+(3*pad))
+  .setSize(bw,200)
+  .setType(ScrollableList.LIST)
+  ;
+
+}
+
+
+
+// When the load file bang is released, open a file explorer window
+void loadFile(){
+  myTerminal.append(theTime() + "Loading file...\n");
+  selectInput("Select script file to load", "fileLoaded");
+}
+
+// See what the user selected as a file
+void fileLoaded(File selection){
+  // If no file, print on screen.
+  if(selection == null){
+    // println("No file selected");
+    myTerminal.append(theTime() + "No file selected\n");
+    myTerminal.scroll(1);
+  } else {
+    // If file, say it and send the file to the dumpFile function
+    // println("File to load: " + selection.getAbsolutePath());
+    fileToDump = selection.getAbsolutePath();   // Save the filename to the global variable
+    myTerminal.append(theTime() + "File to load: " + selection.getAbsolutePath() + "\n");
+    myTerminal.scroll(1);
+    dumpFile(selection.getAbsolutePath());
+    cp5.get(Bang.class,"againFile").show();
+  }
+  // remove the callback from the Bang or else it never lets us go
+  cp5.get(Bang.class,"loadFile").removeCallback();
+}
+
+
+void againFile(){
+  println("fileToDump=" + fileToDump);
+  if(fileToDump.equals("")){
+    // If there's no value in the variable, no file has been dumped
+    myTerminal.append(theTime() + "No file to re-dump\n");
+  } else {
+    // We have a file, re-dump it.
+    println("REDUMPING!!");
+    myTerminal.append(theTime() + "Re-Dumping file " + fileToDump + "\n");
+    myTerminal.scroll(1);
+    dumpFile(fileToDump);
+  }
+  // cp5.get(Bang.class,"aganFile").removeCallback();
 }
 
 
@@ -304,6 +386,7 @@ void saveLog(){
 // dump it.
 void dumpFile(String theFile){
   myTerminal.append(theTime() + "Loading File... \n");
+  myTerminal.scroll(1);
   String theLCFile = theFile.toLowerCase(); // so there's no confustion between JSON and json
 
   // If it's a json let's check if it's the init file to properly send it to the tinyG
@@ -315,6 +398,7 @@ void dumpFile(String theFile){
     myTerminal.append(theTime() + "JSON Loaded... \n");
     delay(250);
     myTerminal.append(theTime() + "Dumping init file... \n");
+    myTerminal.scroll(1);
     // The tinyG doesn't accept JSONArrays as input, so we need to brake it.
     // So lets extract each command as a JSONObject, and
     // then convert it into a String to be sent via Serial to the tinyG
@@ -324,7 +408,8 @@ void dumpFile(String theFile){
       sCommand = sCommand.replaceAll("\\s+", "");               // Clean the string
       println("Init Command # " + i + "> " + jsonObject + "\t | to String > " + sCommand);
       myPort.write(sCommand + "\n");                            // Send it to the tinyG
-      myTerminal.append(theTime() + sCommand + "\n");                       // Display the command on the terminal
+      myTerminal.append(theTime() + sCommand + "\n");           // Display the command on the terminal
+      myTerminal.scroll(1);
       delay(50);
     }
     } else {
@@ -333,12 +418,17 @@ void dumpFile(String theFile){
       String fileLines[] = loadStrings(theFile);
       println("There are " + fileLines.length + " lines in this file");
       myTerminal.append(theTime() + "Sending " + fileLines.length + " lines of code... \n");
+      myTerminal.scroll(1);
 
       for (int i=0 ; i<fileLines.length ; i++){
         println("Going in...");
-        myPort.write(fileLines[i] + "\n");                      // Send the line to the tinyG
+        myPort.write(fileLines[i] + "\n");                                  // Send the line to the tinyG
         myTerminal.append(theTime() + fileLines[i] + "\n");                 // Put the line on the terminal
+        myTerminal.scroll(1);
         delay(100);
+        // We need a longer delay for "move" commands than for the "non-move" commands we send with the INIT file.
+        // This is because of how the TinyG processes the queue. If this delay is made shorter, the tinyG most likely will
+        // crash due to buffer overflow.
       }
     }
   myTerminal.append(theTime() + "File dumped to the tinyG. \n");
@@ -371,4 +461,25 @@ String theTime(){
 
   String timeString = "[" + h + ":" + theMinute + "] ";
   return timeString;
+}
+
+
+
+
+void guiHide(){
+  // Hide all controls until the Serial has been established
+  cp5.get(Bang.class,"Send").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"loadFile").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"againFile").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"saveLog").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"cleanMyFile").hide();                        // hide the "re-dump" bang
+}
+
+void guiShow(){
+  // Hide all controls until the Serial has been established
+  cp5.get(Bang.class,"Send").show();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"loadFile").show();                        // hide the "re-dump" bang
+  // cp5.get(Bang.class,"againFile").show();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"saveLog").show();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"cleanMyFile").show();                        // hide the "re-dump" bang
 }
--- build/source/TinyTerm.java
+++ build/source/TinyTerm.java
@@ -36,10 +36,15 @@ public class TinyTerm extends PApplet {
 //
 // The complete list of tinyG commands and settings can be found @
 // https://github.com/synthetos/TinyG/wiki/TinyG-Configuration-for-Firmware-Version-0.97
+// Or if you have a tinyG connected to the TinyTerm, send the command "$$"
 //
 // The GUI gives you a textArea to show the history of what has been sent and received
 // and a text box to type and inject GCode or tinyG Commands on the fly into the tinyG
 // The interface is created using the controlP5 library.
+//
+// Thanks to http://stackoverflow.com/questions/29107544/serial-com-port-selection-by-dropdownlist-in-processing
+// for the help with adding the Serial port dropdown list
+//
 
 
 // Imports
@@ -49,10 +54,12 @@ public class TinyTerm extends PApplet {
 // GUI
 ControlP5 cp5;
 boolean buttonFlag=false;
+ScrollableList serialPortsList;
 
 // String variables
 String theGCode = "G91 G1 X100 F100\n";     // Whatever you want to have as default text in the textbox
 String jPath = "init.json";                 // the path where the JSON file is
+String fileToDump;                          // String to store filename with absolute path of last dumped file (to quickly re-dump)
 
 // The Init.JSON file to be loaded
 JSONObject initFile;      // This will receive the JSON file as an object
@@ -63,6 +70,8 @@ PrintWriter logFile;
 
 // The serial port:
 Serial myPort;  // Create object from Serial class
+int BAUD_RATE = 9600;
+Boolean serialConnected = false;
 
 //misc variables
 int x = 50;             // Position on the X axis
@@ -76,6 +85,7 @@ int theWidth = 800;     // applet width
 int theHeight = 600;    // applet height
 int pad = 20;           // padding between fields
 int lineCounter = 0;    // linefeed char coming from the serial
+boolean tinyGconnected = true;
 
 Textarea myTerminal;    // CP5 control for the text area
 PFont font;             // the font for the script
@@ -86,21 +96,27 @@ public void settings(){
 }
 
 
-public void setup()
-{
+public void setup(){
   // size(600,600);      //Just if you have a really old version of Processing. Like this laptop.
   // Start the serial
-  // List all the available serial ports, check the terminal window and select find the port# for the tinyG
-  printArray(Serial.list());
-  // Open whichever port the tinyG uses in your computer (8 in mine):
-  myPort = new Serial(this, Serial.list()[2], 9600);
-  myPort.clear();
-
   font = createFont("arial", 20); // big arial font
   startGUI();
   cp5.get(Bang.class,"loadFile").setTriggerEvent(Bang.RELEASE); // make the bang react at release
   // Gui Loaded. Terminal ready
+
+  // List all the available serial ports, check the terminal window and select find the port# for the tinyG
+  String[] portNames = Serial.list();
+  printArray(portNames);
+  // Open whichever port the tinyG uses in your computer (8 in mine):
+  for(int i=0;i<portNames.length;i++){
+    serialPortsList.addItem(portNames[i], i);
+  }
+
+
+  guiHide();
+
   myTerminal.append(theTime() + "Terminal ready... \n");
+  myTerminal.append(theTime() + "Please choose a Serial port to connect to... \n");
   myTerminal.scroll(1);
   textFont(font);
 }
@@ -110,6 +126,7 @@ public void setup()
 public void draw() {
   background(0);  //black BG
   //read response from tinyG
+  if(myPort != null){
   while (myPort.available () > 0) {
     String inBuffer = myPort.readString();
     if (inBuffer != null) {
@@ -129,10 +146,12 @@ public void draw() {
       myTerminal.scroll(1);         // scroll to the bottom of the terminal
     }
   }
+    myPort.clear();
+}
   fill(255);
   stroke(255);
   text("TinyTerm:", x, y-pad);
-  myPort.clear();
+
 }
 
 
@@ -153,13 +172,41 @@ public void controlEvent(ControlEvent theEvent) {
         myTerminal.append(theTime() + "Terminal ready...\n");
       }
       else{
-      myPort.write(theGCode);
-      myTerminal.append(theTime() + theGCode);
-      myTerminal.scroll(1);
-    }
+        myPort.write(theGCode);
+        myTerminal.append(theTime() + theGCode);
+        myTerminal.scroll(1);
+      }
     }
   }
 }
+
+
+
+public void serialports(int n){
+  // Stop any active connections
+  if(myPort != null){
+    myPort.stop();
+    myPort = null;
+  }
+  // open the selected port
+  try{
+    myPort = new Serial(this, Serial.list()[n], BAUD_RATE);
+    serialConnected = true;
+    } catch (Exception e) {
+      System.err.println("Error opening serial port " + myPort);
+      myTerminal.append(theTime() + "Error opening the selected serial port... \n");
+      serialConnected = false;
+      e.printStackTrace();
+    }
+    if(serialConnected) {
+      println("Yay Serial!");
+      myTerminal.append(theTime() + "Connected to Serial on port COM" + n + "\n");
+      guiShow();
+  } else {
+    println("Boo no Serial");
+    myTerminal.append(theTime() + "No Serial connection" + "\n");
+  }
+  }
 
 
 public void Send(){
@@ -189,29 +236,6 @@ public void clear() {
   cp5.get(Textfield.class,"input").clear();
 }
 
-
-
-// When the load file bang is released, open a file explorer window
-public void loadFile(){
-  myTerminal.append(theTime() + "Loading file...\n");
-  selectInput("Select script file to load", "fileLoaded");
-}
-
-// See what the user selected as a file
-public void fileLoaded(File selection){
-  // If no file, print on screen.
-  if(selection == null){
-    // println("No file selected");
-    myTerminal.append(theTime() + "No file selected\n");
-  } else {
-    // If file, say it and send the file to the dumpFile function
-    // println("File to load: " + selection.getAbsolutePath());
-    myTerminal.append(theTime() + "File to load: " + selection.getAbsolutePath() + "\n");
-    dumpFile(selection.getAbsolutePath());
-  }
-  // remove the callback from the Bang or else it never lets us go
-  cp5.get(Bang.class,"loadFile").removeCallback();
-}
 
 
 // Let's work on the GUI
@@ -272,6 +296,15 @@ public void startGUI(){
   .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
   ;
 
+  // create a new button to re-dump the last opened file to the tinyG
+  cp5.addBang("againFile")
+  .setCaptionLabel("Re-Dump File")
+  .setPosition(x+taw+pad, y+tah+pad)
+  .setSize(bw, tfh)
+  .setColorBackground(color(180,40,50))
+  .setColorActive(color(180,40,50))
+  .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
+  ;
 
   // create a new button to save the log
   cp5.addBang("saveLog")
@@ -295,6 +328,56 @@ public void startGUI(){
   .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
   ;
 
+  serialPortsList = cp5.addScrollableList("serialports")
+  .setPosition(x+taw+pad,y+(3*sbh)+(3*pad))
+  .setSize(bw,200)
+  .setType(ScrollableList.LIST)
+  ;
+
+}
+
+
+
+// When the load file bang is released, open a file explorer window
+public void loadFile(){
+  myTerminal.append(theTime() + "Loading file...\n");
+  selectInput("Select script file to load", "fileLoaded");
+}
+
+// See what the user selected as a file
+public void fileLoaded(File selection){
+  // If no file, print on screen.
+  if(selection == null){
+    // println("No file selected");
+    myTerminal.append(theTime() + "No file selected\n");
+    myTerminal.scroll(1);
+  } else {
+    // If file, say it and send the file to the dumpFile function
+    // println("File to load: " + selection.getAbsolutePath());
+    fileToDump = selection.getAbsolutePath();   // Save the filename to the global variable
+    myTerminal.append(theTime() + "File to load: " + selection.getAbsolutePath() + "\n");
+    myTerminal.scroll(1);
+    dumpFile(selection.getAbsolutePath());
+    cp5.get(Bang.class,"againFile").show();
+  }
+  // remove the callback from the Bang or else it never lets us go
+  cp5.get(Bang.class,"loadFile").removeCallback();
+}
+
+
+public void againFile(){
+  println("fileToDump=" + fileToDump);
+  if(fileToDump.equals("")){
+    // If there's no value in the variable, no file has been dumped
+    myTerminal.append(theTime() + "No file to re-dump\n");
+  } else {
+    // We have a file, re-dump it.
+    println("REDUMPING!!");
+    myTerminal.append(theTime() + "Re-Dumping file " + fileToDump + "\n");
+    myTerminal.scroll(1);
+    dumpFile(fileToDump);
+  }
+  // cp5.get(Bang.class,"aganFile").removeCallback();
 }
 
 
@@ -322,6 +405,7 @@ public void saveLog(){
 // dump it.
 public void dumpFile(String theFile){
   myTerminal.append(theTime() + "Loading File... \n");
+  myTerminal.scroll(1);
   String theLCFile = theFile.toLowerCase(); // so there's no confustion between JSON and json
 
   // If it's a json let's check if it's the init file to properly send it to the tinyG
@@ -333,6 +417,7 @@ public void dumpFile(String theFile){
     myTerminal.append(theTime() + "JSON Loaded... \n");
     delay(250);
     myTerminal.append(theTime() + "Dumping init file... \n");
+    myTerminal.scroll(1);
     // The tinyG doesn't accept JSONArrays as input, so we need to brake it.
     // So lets extract each command as a JSONObject, and
     // then convert it into a String to be sent via Serial to the tinyG
@@ -342,7 +427,8 @@ public void dumpFile(String theFile){
       sCommand = sCommand.replaceAll("\\s+", "");               // Clean the string
       println("Init Command # " + i + "> " + jsonObject + "\t | to String > " + sCommand);
       myPort.write(sCommand + "\n");                            // Send it to the tinyG
-      myTerminal.append(theTime() + sCommand + "\n");                       // Display the command on the terminal
+      myTerminal.append(theTime() + sCommand + "\n");           // Display the command on the terminal
+      myTerminal.scroll(1);
       delay(50);
     }
     } else {
@@ -351,12 +437,17 @@ public void dumpFile(String theFile){
       String fileLines[] = loadStrings(theFile);
       println("There are " + fileLines.length + " lines in this file");
       myTerminal.append(theTime() + "Sending " + fileLines.length + " lines of code... \n");
+      myTerminal.scroll(1);
 
       for (int i=0 ; i<fileLines.length ; i++){
         println("Going in...");
-        myPort.write(fileLines[i] + "\n");                      // Send the line to the tinyG
+        myPort.write(fileLines[i] + "\n");                                  // Send the line to the tinyG
         myTerminal.append(theTime() + fileLines[i] + "\n");                 // Put the line on the terminal
+        myTerminal.scroll(1);
         delay(100);
+        // We need a longer delay for "move" commands than for the "non-move" commands we send with the INIT file.
+        // This is because of how the TinyG processes the queue. If this delay is made shorter, the tinyG most likely will
+        // crash due to buffer overflow.
       }
     }
   myTerminal.append(theTime() + "File dumped to the tinyG. \n");
@@ -389,6 +480,27 @@ public String theTime(){
 
   String timeString = "[" + h + ":" + theMinute + "] ";
   return timeString;
+}
+
+
+
+
+public void guiHide(){
+  // Hide all controls until the Serial has been established
+  cp5.get(Bang.class,"Send").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"loadFile").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"againFile").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"saveLog").hide();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"cleanMyFile").hide();                        // hide the "re-dump" bang
+}
+
+public void guiShow(){
+  // Hide all controls until the Serial has been established
+  cp5.get(Bang.class,"Send").show();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"loadFile").show();                        // hide the "re-dump" bang
+  // cp5.get(Bang.class,"againFile").show();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"saveLog").show();                        // hide the "re-dump" bang
+  cp5.get(Bang.class,"cleanMyFile").show();                        // hide the "re-dump" bang
 }
 PrintWriter output;
 String loadFile, saveFile;
